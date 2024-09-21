4666c9a35e9f6e19a01f5c1f49f0eb4d7da378b8
kehnniey <kehnniey@gmail.com>
2024-09-21 19:16:42 -0400

    prep 1 updated

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+       
+         // Prompt user for first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Prompt user for last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Display the formatted output
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
